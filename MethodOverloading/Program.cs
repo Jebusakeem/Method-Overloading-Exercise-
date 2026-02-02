177c6b4e84d5ee088b66ddbd9c18b6437e7aa49c
Briun Greene <harmonickarma789@gmail.com>
2026-02-01 19:53:18 -0500

    Overloading methods exercise complete

--- MethodOverloading/Program.cs
+++ MethodOverloading/Program.cs
@@ -1,10 +1,36 @@
-﻿namespace MethodOverloading
+﻿using System.ComponentModel;
+
+namespace MethodOverloading
 {
     public class Program
     {
         static void Main(string[] args)
         {
             Console.WriteLine("Hello, World!");
+        }
+
+        public static int Add(int a, int b)
+        {
+            return a + b;
+        }
+
+        public static decimal Add(decimal a, decimal b)
+        {
+            return a + b;
+        }
+
+        public static string Add(int a, int b, bool dollar)
+        {
+            if (a + b > 1)
+            {
+                dollar = false;
+                return $" {a + b} dollars";
+                }
+            if (a + b == 1)
+            {
+                dollar = true;
+                return $"{a + b} dollar";
+                }
         }
     }
 }
